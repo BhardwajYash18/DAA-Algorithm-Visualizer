60a74b7fdec6820a8f47972f4411054b574c93ec
Yash Bhardwaj <yashb18.04@gmail.com>
2026-04-16 01:31:02 +0530

    Update Knapsack.jsx

--- frontend/src/pages/Knapsack.jsx
+++ frontend/src/pages/Knapsack.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const API = 'http://localhost:5000/api'
+const API = 'https://daa-algorithm-visualizer.onrender.com'
 
 const DEFAULT_ITEMS = [
   { item: 'A', weight: '2', value: '10' },
